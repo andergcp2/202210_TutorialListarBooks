aafbde349994454a04d3c8e100f964455b1329c4
acastiblanco <ag.castiblanco1207@uniandes.edu.co>
2022-05-01 23:37:08 -0500

    Proyecto parcial

---
+++ src/app/cafe/cafe.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListarCafeComponent } from './listar-cafe/listar-cafe.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [ListarCafeComponent],
+  declarations: [ListarCafeComponent],
+})
+export class CafeModule {}
