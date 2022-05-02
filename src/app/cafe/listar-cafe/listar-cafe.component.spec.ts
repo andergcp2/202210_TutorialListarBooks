3d43c3fbd54b825e6d208705f3439e0fe05a2766
acastiblanco <ag.castiblanco1207@uniandes.edu.co>
2022-05-01 23:47:37 -0500

    Agregando prueba al componente

--- src/app/cafe/listar-cafe/listar-cafe.component.spec.ts
+++ src/app/cafe/listar-cafe/listar-cafe.component.spec.ts
@@ -4,25 +4,39 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { ListarCafeComponent } from './listar-cafe.component';
+import { Cafe } from '../cafe';
+import { faker } from 'faker';
 
 describe('ListarCafeComponent', () => {
   let component: ListarCafeComponent;
   let fixture: ComponentFixture<ListarCafeComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListarCafeComponent ]
-    })
-    .compileComponents();
+      declarations: [ListarCafeComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarCafeComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(1, 'Cafe 1', 'Tipo 1', 'región 1', 'sabor 1', 1000, 'urlFake'),
+      new Cafe(2, 'Cafe 2', 'Tipo 1', 'región 1', 'sabor 1', 1000, 'urlFake'),
+      new Cafe(3, 'Cafe 3', 'Tipo 1', 'región 1', 'sabor 1', 1000, 'urlFake'),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('should create the component listar-cafe', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have 3 imgs elements ', () => {
+    expect(debug.query(By.css('img')).attributes['alt']).toEqual(
+      component.cafes[0].imagen
+    );
   });
 });
